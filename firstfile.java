0ecc3eb2a92ccfc3d2c2e10d2a6803d9c9df532c
JohnWesleyK <johnwesleygithub@gmail.com>
2021-04-14 11:50:45 +0530

    Part 3 almost done

--- firstfile.java
+++ firstfile.java
@@ -6,11 +6,25 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.xml.crypto.Data;
+
+import java.awt.event.*;
+import java.awt.*;
 
 /**
  * firstfile
  */
+
 public class firstfile {
 
     public static String[] getColumnNames(File filename) throws IOException {
@@ -18,7 +32,7 @@ public class firstfile {
         String st;
         while ((st = br.readLine()) != null) {
             String[] line = st.split(",");
-            // br.close();
+            br.close();
             return line;
         }
         br.close();
@@ -27,10 +41,8 @@ public class firstfile {
 
     public static int getLinesCount(File filename) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filename));
-        String st;
         int count = 0;
-        while ((st = br.readLine()) != null) {
-            String[] line = st.split(",");
+        while ((br.readLine()) != null) {
             count++;
         }
         br.close();
@@ -38,13 +50,13 @@ public class firstfile {
     }
 
     public static boolean isTimeValid(String time) {
-        final int MAX_HOURS = 24;
+        final int MAX_HOURS = 23;
         final int MAX_MINUTES = 59;
         final int MAX_SECONDS = 59;
 
-        final int MIN_HOURS = 00;
-        final int MIN_MINUTES = 00;
-        final int MIN_SECONDS = 00;
+        // final int MIN_HOURS = 00;
+        // final int MIN_MINUTES = 00;
+        // final int MIN_SECONDS = 00;
 
         // System.out.println("\n Input time - "+time);
         String time_without_space = time.replaceAll("\\s", "");
@@ -59,7 +71,7 @@ public class firstfile {
             minutes = Integer.parseInt(individualParts[1]);
             seconds = Integer.parseInt(individualParts[2]);
         } catch (Exception e) {
-            System.err.println(e);
+            // System.err.println(e);
             return false;
         }
 
@@ -99,6 +111,180 @@ public class firstfile {
         return validTimes;
     }
 
+    public static Map<String, ArrayList<String>> createSortedArrivalTimeMap(ArrayList<String> validTimes) {
+        int indexOfArrivalTime = 1;
+        Map<String, ArrayList<String>> Time_Line = new TreeMap<>();
+
+        for (int i = 0; i < validTimes.size(); i++) {
+            String line = validTimes.get(i);
+            String[] values = line.split(",");
+            String arrivalTime = values[indexOfArrivalTime];
+
+            // arrivalTime.replaceAll("\\s", ""); // removing spaces
+
+            Time_Line.computeIfAbsent(arrivalTime, k -> new ArrayList<>()).add(line);
+        }
+        return Time_Line;
+    }
+
+    public static void printTripDetailsFromList(ArrayList<String> list_line) {
+
+        String ColumnLabel0 = "trip_id - ";
+        String ColumnLabel1 = " arrival_time - ";
+        String ColumnLabel2 = " departure_time - ";
+        String ColumnLabel3 = " stop_id - ";
+        String ColumnLabel4 = " stop_sequence - ";
+        String ColumnLabel5 = " stop_headsign - ";
+        String ColumnLabel6 = " pickup_type - ";
+        String ColumnLabel7 = " drop_off_type - ";
+        String ColumnLabel8 = " shape_dist_traveled - ";
+
+        for (String line : list_line) {
+            String lineWithoutBraces = line.substring(1, line.length() - 1);
+            String[] values = lineWithoutBraces.split(",");
+            try {
+                String s = values[8];
+            } catch (ArrayIndexOutOfBoundsException aio) {
+                // tripsData[i][j] = "";
+                values[8] = " ";
+            }
+            System.out.println(ColumnLabel0 + values[0] + ColumnLabel1 + values[1] + ColumnLabel2 + values[2]
+                    + ColumnLabel3 + values[3] + ColumnLabel4 + values[4] + ColumnLabel5 + values[5] + ColumnLabel6
+                    + values[6] + ColumnLabel7 + values[7] + ColumnLabel8 + values[8]);
+        }
+
+    }
+
+    public static void table_test() throws IOException {
+
+    }
+
+    public static void Part_3_GUI() throws IOException {
+
+        File stop_times = new File("inputs/stop_times.txt");
+        ArrayList<String> validTimes = getValidTimes(stop_times);
+        Map<String, ArrayList<String>> arrivalTimes_String = createSortedArrivalTimeMap(validTimes);
+
+        JFrame f = new JFrame("PART 3 GUI");
+
+        String[] columnLabels = getColumnNames(stop_times);
+        String[][] tableData = new String[10][10];
+
+        DefaultTableModel dtm = new DefaultTableModel(tableData, columnLabels);
+        JTable table = new JTable(dtm);
+        JScrollPane scrollPane = new JScrollPane(table);
+        // JScrollBar vScroll = scrollPane.getVerticalScrollBar();
+        table.setLayout(new BorderLayout());
+
+        int N_ROWS = tableData.length;
+
+        Dimension d = new Dimension(800, N_ROWS * table.getRowHeight());
+        table.setPreferredScrollableViewportSize(d);
+        TableColumn column = null;
+        column = table.getColumnModel().getColumn(0);
+        column.setPreferredWidth(columnLabels[0].length() * 15);
+        for (int i = 1; i < columnLabels.length; i++) {
+            column = table.getColumnModel().getColumn(i);
+            column.setPreferredWidth(columnLabels[i].length() * 10);
+        }
+
+        for (int i = 0; i < N_ROWS; i++) {
+            dtm.addRow(tableData[i]);
+        }
+
+        // JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.add(scrollPane, BorderLayout.CENTER);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        // f.setVisible(true);
+
+        JButton b = new JButton("Show");
+        b.setBounds(325, 300, 90, 20);
+
+        String hours[] = { "  0", "  1", "  2", "  3", "  4", "  5", "  6", "  7", "  8", "  9", " 10", " 11", " 12",
+                " 13", " 14", " 15", " 16", " 17", " 18", " 19", " 20", " 21", " 22", " 23" };
+
+        String minutes_seconds[] = { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13",
+                "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47",
+                "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59" };
+
+        final JComboBox hoursInput = new JComboBox(hours);
+        hoursInput.setBounds(25, 300, 90, 20);
+
+        final JComboBox minutesInput = new JComboBox(minutes_seconds);
+        minutesInput.setBounds(125, 300, 90, 20);
+
+        final JComboBox secondsInput = new JComboBox(minutes_seconds);
+        secondsInput.setBounds(225, 300, 90, 20);
+
+        f.add(hoursInput);
+        f.add(minutesInput);
+        f.add(secondsInput);
+
+        final JLabel label = new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setSize(500, 500);
+
+        final JLabel error_label = new JLabel();
+        error_label.setHorizontalAlignment(JLabel.CENTER);
+        error_label.setSize(500, 600);
+
+        f.add(label);
+
+        f.add(b);
+
+        // f.add(sp);
+        // f.add(table);
+
+        f.setLayout(null);
+        f.setSize(800, 600);
+        f.setVisible(true);
+
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String time = hoursInput.getItemAt(hoursInput.getSelectedIndex()) + ":"
+                        + minutesInput.getItemAt(minutesInput.getSelectedIndex()) + ":"
+                        + secondsInput.getItemAt(secondsInput.getSelectedIndex());
+                String displayData = "Time you've input is " + time;
+                label.setText(displayData);
+
+                System.out.println(displayData);
+
+                // String timeForMap = "\\s+".concat(time);
+                ArrayList<String> lines = new ArrayList<String>();
+                try {
+                    lines = arrivalTimes_String.get(time);
+                } catch (Exception er) {
+                    String errorMessage = "Sorry, There doesn't seem to be any buses at the time you have selected";
+                    error_label.setText(errorMessage);
+                    System.out.println(errorMessage);
+                }
+
+                printTripDetailsFromList(lines);
+                String[][] tripsData = new String[lines.size()][columnLabels.length];
+                for (int i = 0; i < tripsData.length; i++) {
+                    String line = lines.get(i);
+                    String lineWithoutBraces = line.substring(1, line.length() - 1);
+                    String[] values = lineWithoutBraces.split(",");
+                    for (int j = 0; j < tripsData[i].length; j++) {
+                        try {
+                            tripsData[i][j] = values[j];
+                        } catch (ArrayIndexOutOfBoundsException aio) {
+                            tripsData[i][j] = " ";
+                        }
+                        // System.out.println(tripsData[i][j]);
+                    }
+                }
+                dtm.setDataVector(tripsData, columnLabels);
+                System.out.println("-------endline-------");
+            }
+        });
+    }
+
     public static void main(String[] args) throws IOException {
         // File stops = new File("inputs/stops.txt");
         // File transfers = new File("inputs/transfers.txt");
@@ -133,12 +319,54 @@ public class firstfile {
         // boolean bool1 = isTimeValid(time1);
         // boolean bool2 = isTimeValid(time2);
 
-        File stop_times = new File("inputs/stop_times.txt");
-        ArrayList<String> validTimes = getValidTimes(stop_times);
+        // File stop_times = new File("inputs/stop_times.txt");
+        // ArrayList<String> validTimes = getValidTimes(stop_times);
 
-        System.out.println("FirstLine " + validTimes.get(0));
-        System.out.println("SecondLine " + validTimes.get(1));
+        // System.out.println("FirstLine " + validTimes.get(0));
+        // System.out.println("SecondLine " + validTimes.get(1));
 
-        System.out.println("valid elements - " + validTimes.size());
+        // System.out.println("valid elements - " + validTimes.size());
+
+        // Map<String, ArrayList<String>> arrivalTimes_String =
+        // createSortedArrivalTimeMap(validTimes);
+
+        // for (String time : arrivalTimes_String.keySet()) {
+        // System.out.println(" Time " + time);
+        // " Time " " 22:27:37"
+        // ArrayList<String> lines = arrivalTimes_String.get(time);
+        // for (String line : lines) {
+        // System.out.println(line);
+        // }
+        // System.out.println("-------endline-------");
+        // }
+
+        // String time1 = "4:27:02";
+        // String time2 = "24:59:59";
+        // String time3 = "5:25:50";
+
+        // System.out.println(time2);
+        // ArrayList<String> lines = arrivalTimes_String.get(time2);
+        // printTripDetailsFromList(lines);
+        // for (String line : lines) {
+        // System.out.println(line);
+        // }
+        // System.out.println("-------endline-------");
+
+        // System.out.println(time3);
+        // lines = arrivalTimes_String.get(time3);
+        // printTripDetailsFromList(lines);
+        // for (String line : lines) {
+        // System.out.println(line);
+        // }
+        // System.out.println("-------endline-------");
+
+        // System.out.println(time1);
+        // lines = arrivalTimes_String.get(time1);
+        // printTripDetailsFromList(lines);
+
+        Part_3_GUI();
+        // table_test();
+        // SwingUtilities.invokeLater(table_test());
+
     }
 }
